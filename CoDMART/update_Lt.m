bb21955485ba997ba6df6aa8dd15905cf3c2fa7f
EsraaAlsharoa <95857663+EsraaAlsharoa@users.noreply.github.com>
2025-04-17 18:13:11 +0300

    Update update_Lt.m

--- CoDMART/update_Lt.m
+++ CoDMART/update_Lt.m
@@ -1,4 +1,25 @@
 function [Lt] = update_Lt(Lt,At,St,Xt,Yt,Wt,Ut_indv,lambda2,mu)
+% Definition:
+%     This code is used to update the low-rank component at time point t
+
+%   References:
+%       [1] E. Al sharoa, M. Alwardat and S. Aviyente. "Community Detection 
+%        in Multi-Aspect Functional Brain Networks: Robust Tensor 
+%        Decomposition Approach"
+
+%   Author: Esraa Al-sharoa 
+%   Address: Jordan University of Science and Technoloogy, EE
+%   email: emalsharoa@just.edu.jo
+
+%   Author: Mohammad Al-wardat
+%   Address: Michigan State University, ECE
+%   email: alwardat@msu.edu
+%   Website: GitHub
+%   Date: 12-Feb-2024; Last revision: 03-April-2025
+%
+%   Copyright (c) 2025, Esraa Al-sharoa and Mohammad Al-wardat
+
+%   All rights reserved.
 
 %% Check the variable type of Ut_indv
 tf = isa(Ut_indv,'cell');
