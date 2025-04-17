199e6fa4b8bdb604c4f3043fa6cbbd6ed0ea46b2
EsraaAlsharoa <95857663+EsraaAlsharoa@users.noreply.github.com>
2025-04-17 18:12:14 +0300

    Update updateUt_c.m

--- CoDMART/updateUt_c.m
+++ CoDMART/updateUt_c.m
@@ -1,4 +1,27 @@
 function [Ut_c] = updateUt_c(Lt,Ut_indv,Ut1_c,wt,lambda3,lambda4,numClust)
+% Definition:
+%     This code is used to update the common subspace across all the layers at time point t
+
+%   References:
+%       [1] E. Al sharoa, M. Alwardat and S. Aviyente. "Community Detection 
+%        in Multi-Aspect Functional Brain Networks: Robust Tensor 
+%        Decomposition Approach"
+
+%   Author: Esraa Al-sharoa 
+%   Address: Jordan University of Science and Technoloogy, EE
+%   email: emalsharoa@just.edu.jo
+
+%   Author: Mohammad Al-wardat
+%   Address: Michigan State University, ECE
+%   email: alwardat@msu.edu
+%   Website: GitHub
+%   Date: 12-Feb-2024; Last revision: 03-April-2025
+%
+%   Copyright (c) 2025, Esraa Al-sharoa and Mohammad Al-wardat
+
+%   All rights reserved.
+
+
 
 tf = isa(Ut_indv,'cell');
 [n,~,M]=size(Lt);
