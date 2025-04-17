cd8057b1bc4237a3ff2e3309e43b02572d10b662
EsraaAlsharoa <95857663+EsraaAlsharoa@users.noreply.github.com>
2025-04-17 18:11:28 +0300

    Update updateUt_indv_1stPoint.m

--- CoDMART/updateUt_indv_1stPoint.m
+++ CoDMART/updateUt_indv_1stPoint.m
@@ -1,4 +1,25 @@
 function [Ut_indv] = updateUt_indv_1stPoint(Lt,wt,Ut_c,numClust,lambda2,lambda4)
+% Definition:
+%     This code is used to update the individual subspaces of the different layers at the first time point 
+
+%   References:
+%       [1] E. Al sharoa, M. Alwardat and S. Aviyente. "Community Detection 
+%        in Multi-Aspect Functional Brain Networks: Robust Tensor 
+%        Decomposition Approach"
+
+%   Author: Esraa Al-sharoa 
+%   Address: Jordan University of Science and Technoloogy, EE
+%   email: emalsharoa@just.edu.jo
+
+%   Author: Mohammad Al-wardat
+%   Address: Michigan State University, ECE
+%   email: alwardat@msu.edu
+%   Website: GitHub
+%   Date: 12-Feb-2024; Last revision: 03-April-2025
+%
+%   Copyright (c) 2025, Esraa Al-sharoa and Mohammad Al-wardat
+
+%   All rights reserved.
 
 
 
